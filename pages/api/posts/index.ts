238e68d9a4828e1be02b398fe1a1649592680451
Maxime.Antoine <maximeantoine1997@gmail.com>
2022-10-24 22:29:03 -0400

    fix api request not working

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -4,11 +4,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     switch (req.method) {
         case "GET": {
-            const posts: { title: string }[] = await axios.get("https://jsonplaceholder.typicode.com/users/1/posts")
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users/1/posts")
 
+            if (response.status !== 200) return res.status(400).send("Could not fetch data...")
+
+            const posts = response.data as { title: string }[]
             const { q } = req.query
 
-            return posts.filter(post => post.title.includes(q as string))
+            console.log(posts)
+
+            return res.status(200).send({ posts: posts.filter(post => post.title.includes(q as string)) })
         }
         case "PUT":
             return res.status(400).send("not implemented")
